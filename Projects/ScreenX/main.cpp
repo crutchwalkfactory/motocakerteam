81ae1128670d8000cab6ab1992b6402fccb5af4f
cepiperez <cepiperez@gmail.com>
2009-09-18 15:27:46 +0000

    Desktop enhancement project

---
+++ Projects/ScreenX/main.cpp
@@ -0,0 +1,29 @@
+#include "ScreenX.h"
+#include <ZApplication.h>
+#include <qpixmap.h>
+#include <ZConfig.h>
+
+ScreenX *screenx;
+ZApplication* app;
+
+int main ( int argc, char **argv )
+{
+  app = new ZApplication ( argc, argv );
+  screenx = new ScreenX ( NULL, NULL );
+
+	QString Fval1; ZConfig Fini1(QString("/ezxlocal/download/appwrite/setup/ezx_theme.cfg"), false);
+	Fval1 = Fini1.readEntry(QString("THEME_GROUP_LOG"), QString("CurrentTheme"), "");
+	QString Fval2; ZConfig Fini2(Fval1, false);
+	Fval2 = Fini2.readEntry(QString("THEME_CONFIG_TABLE"), QString("WallPaper"), "");
+
+	QPixmap f1; f1.load(Fval2);
+	app->setAppFSBA( f1, 100, 0 );
+
+
+  int ret = app->exec();
+  delete screenx;
+  screenx = NULL;
+  delete app;
+  app = NULL;
+  return ret;
+}
